fec7e0983412e9439232d43d80588d0905d01c5b
EagleCollin <scoutcollin97@gmail.com>
2022-04-01 16:01:22 -0500

    Completed Exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
+
 export class AppComponent {
-  title = 'angular-ngFor-input-decorators';
+
+
+  users: user[]=[{username:'James', id:143}, {username:'Nicole', id:432}]
 }
+
+export interface user{
+  username: string;
+  id: number;
+};
